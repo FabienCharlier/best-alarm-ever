4c8727d1b62a906d0da9adb543b0743216870d7c
ElRequinDesHerbos <dragonberserk1er@gmail.com>
2023-07-30 11:39:03 +0200

    début du dev

--- reveil/App.js
+++ reveil/App.js
@@ -1,14 +1,26 @@
+import {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
 
 export default function App() {
+
+  const [text, setText] = useState('Grayou')
+  const [date, setDate] = useState(new Date(Date.now()))
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text> Hello </Text>
+        <TextInput value={text} onChangeText={setText}/>
+        <RNDateTimePicker value={date} />
+        <StatusBar style="auto" />
+      </View>
+      <Text> {text} </Text>
+    </>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +30,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
